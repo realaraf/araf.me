55418d972eb61c85d3b3203e62b77d2c1d5b08f0
Araf <97058316+realaraf@users.noreply.github.com>
2025-09-07 12:58:29 +0600

    404 page

---
+++ 404.php
@@ -0,0 +1,12 @@
+<?php get_header(); ?>
+
+<div class="max-w-[800px] mx-5 sm:mx-auto">
+
+  <h1>404</h1>
+  <p class="hero">Sorry, the page you are looking for could not be found. <br/> You can go back to the <a href="<?php echo home_url(); ?>">homepage</a>.</p>
+
+</div>
+
+<?php get_footer(); ?>
+
+    
