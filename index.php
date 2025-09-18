c8aa91f2d46b4d0cfbcbf04d3d5421baa480a222
Araf <97058316+realaraf@users.noreply.github.com>
2025-09-18 12:40:31 +0600

    Link post format updated

--- index.php
+++ index.php
@@ -4,30 +4,38 @@
 
   <h2>Blog Posts</h2>
 
-  <ul class="ps-0 posts">
-  <!-- Post Loop Start -->
-  <?php 
-  if ( have_posts() ) :
-      while ( have_posts() ) : the_post();
-  ?>
+    <?php
+    $args = array(
+        'post_type'      => 'post',
+        'posts_per_page' => 100,
+        'orderby'        => 'date',
+        'order'          => 'DESC',
+    );
 
-      <li>
-        <a href="<?php echo get_the_permalink(); ?>"><?php echo get_the_title(); ?></a> 
-        <span><?php echo get_the_date('M j, Y'); ?></span>
-      </li>   
-      
-      <!-- Pagination Start -->
-          <?php //wpstarter_pagination() ?>
-      <!-- Pagination End -->
+    $query = new WP_Query( $args );
 
-  <?php
-      endwhile;
-  else :
-      _e( 'Sorry, no posts were found.', 'wpstarter' );
-  endif;
-  ?>
-  <!-- Post Loop End -->
-  </ul>
+    if ( $query->have_posts() ) {
+        echo '<ul class="ps-0 posts">';
+        while ( $query->have_posts() ) {
+            $query->the_post();
+            $format = get_post_format();
+
+            // For standard posts, get_post_format() returns false
+            if ( ! $format || $format === 'standard' ) : 
+        ?>
+            <li>
+                <a href="<?php echo esc_url( get_permalink() ); ?>">
+                    <?php echo esc_html( get_the_title() ); ?>
+                </a>
+                <span><?php echo esc_html( get_the_date( 'M j, Y' ) ); ?></span>
+            </li>
+        <?php 
+            endif;
+        }
+        echo '</ul>';
+        wp_reset_postdata();
+    }
+    ?>
 
 </div>
 
